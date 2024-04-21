c44c99755e0c25cf39f33cbf50a9e14bc9114a5b
JanBenda <jan.benda97@seznam.cz>
2024-04-21 10:43:43 +0200

    Health checks moved to separated function.

---
+++ src/microservices/Catalog.API.Write/Extensions/HealthChecksExtension.cs
@@ -0,0 +1,32 @@
+
+using System.Text;
+
+namespace Catalog.API.Write.Extensions;
+
+public static class HealthChecksExtension
+{
+    public static void AddHealthCheckExtensions(this IServiceCollection services, IConfiguration configuration)
+    {
+        var rabbitConnection = new StringBuilder();
+        rabbitConnection
+            .Append("amqp://")
+            .Append(configuration["RabbitMQ:Username"] ?? "user")
+            .Append(":")
+            .Append(configuration["RabbitMQ:Password"] ?? "")
+            .Append("@")
+            .Append(configuration["RabbitMQ:Hostname"] ?? "localhost")
+            .Append("/");
+
+        services.AddHealthChecks()
+            .AddNpgSql(
+                connectionString: configuration["PostgreSQL:ConnectionString"] ?? "",
+                tags: new string[] { "CatalogDatabase", "PostgreSQL" }
+            ).AddRedis(
+                configuration["RedisCache:ConnectionString"] ?? "",
+                tags: new string[] { "RedisCacheCatalogAPI", "Redis" }
+            ).AddRabbitMQ(
+                rabbitConnectionString: rabbitConnection.ToString(),
+                tags: new string[] { "EventBus", "RabbitMQ" }
+            );
+    }
+}
